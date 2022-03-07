7b2ab141ddd49b275ae5ddbc34365b289860e622
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-03-07 21:49:54 +0900

    Update ParamReaderInterface doc comment

--- src/ParamReaderInterface.php
+++ src/ParamReaderInterface.php
@@ -7,7 +7,7 @@ namespace Koriym\ParamReader;
 use ReflectionParameter;
 
 /**
- * An attribute/annotation reader for method parameters
+ * Interface for annotation/attribute readers for method parameter
  *
  * Attempts to read the attribute(s) of the parameter,
  * and if not successful, attempts to read the annotation(s) of the property of the same parameter name.
@@ -17,18 +17,23 @@ use ReflectionParameter;
 interface ParamReaderInterface
 {
     /**
-     * Read the parameter attribute or annotation
+     * Gets the attriburtes/annotations applied to a method parameter.
      *
-     * @param class-string<T> $class
+     * @param ReflectionParameter $param The ReflectionParameter of the parameter
+     *                                   from which the annotations/attributes should be read.
      *
-     * @return T|null
-     */
-    public function getParametrAnnotation(ReflectionParameter $param, string $class): ?object;
-
-    /**
-     * Read the parameter attributes or annotations
-     *
-     * @return array<T>
+     * @return array<T> An array of Annotations/Attributes.
      */
     public function getParametrAnnotations(ReflectionParameter $param): array;
+
+    /**
+     * Gets a method parameter attriburte/annotation
+     *
+     * @param ReflectionParameter $param      The ReflectionParameter of the parameter
+     *                                        from which the annotations/attributes should be read.
+     * @param class-string<T>     $annotation
+     *
+     * @return T|null The Annotation/Attribute or NULL, if the requested annotation does not exist.
+     */
+    public function getParametrAnnotation(ReflectionParameter $param, string $annotation): ?object;
 }
